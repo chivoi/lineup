47664af44d6cb5bd9ffa054d45002fbaab34b9f4
chivoi <a.lastovirya@gmail.com>
2021-03-08 18:11:10 +1100

    adds default value to request status, adds enum values for status in request model, adds show page for requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :gig
   belongs_to :profile
+  enum status: {pending: 0, accepted: 1, declined: 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_064327) do
+ActiveRecord::Schema.define(version: 2021_03_08_070806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_064327) do
   create_table "requests", force: :cascade do |t|
     t.bigint "gig_id", null: false
     t.bigint "profile_id", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "message"

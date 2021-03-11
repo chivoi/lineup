87c3f405bea39859b050081a1e9749eedd2b0152
chivoi <a.lastovirya@gmail.com>
2021-03-11 16:42:11 +1100

    sets up requests page with sent and received requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,14 +1,25 @@
 class RequestsController < ApplicationController
+  before_action :set_gig, only: [:new]
+  before_action :set_request, only: [:new]
+  before_action :set_host, only: [:new]
+  before_action :set_user_request, only: [:new]
 
   def new
-    @gig = Gig.find(params[:id])
+    @gig.filled = true
+    @gig.save
     @request = Request.create(user_id: current_user.id, gig_id: @gig.id, host_id: @gig.user_id)
+  end
+
+  def show
+    @requested_gigs = current_user.requests.all
+    requests = Request.all
+    @requests_received = requests.where(host_id:current_user.id)
   end
 
   private
 
   def set_request
-    @request = Request.find(params[:id])
+    @request = Request.where(gig_id:params[:id])
   end
 
   def request_params
@@ -16,11 +27,19 @@ class RequestsController < ApplicationController
   end
 
   def set_user_request
-    @request = current_user.requests.find_by_id(params[:id])
+    @request = current_user.requests.where(gig_id:params[:id])
     if @request == nil
       flash[:alert] = "You don't have permission to do that"
       redirect_to gigs_path
     end
+  end
+
+  def set_host
+    @host = Gig.find(params[:id]).user.profile
+  end
+
+  def set_gig
+    @gig = Gig.find(params[:id])
   end
 
 end

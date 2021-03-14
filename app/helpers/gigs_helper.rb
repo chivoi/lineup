04506896fd4d517cdb49aaa5be617ea2bd75e871
chivoi <a.lastovirya@gmail.com>
2021-03-14 21:58:50 +1100

    styles request cards, donation button, badges

--- app/helpers/gigs_helper.rb
+++ app/helpers/gigs_helper.rb
@@ -1,11 +1,11 @@
 module GigsHelper
   def select_badge_color(style_id)
     if style_id % 3 == 0
-      return "badge-primary"
+      return "badge-yellow"
     elsif style_id % 2 == 0
-      return "badge-dark"
+      return "badge-black"
     else
-      return "badge-info"
+      return "badge-red"
     end
   end
 end

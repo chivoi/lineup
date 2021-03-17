501986010bde2d5e0d86606a4571db622a8e4f65
chivoi <a.lastovirya@gmail.com>
2021-03-17 12:09:01 +1100

    removes read column from messages

---
+++ db/migrate/20210317010749_remove_read_from_message.rb
@@ -0,0 +1,5 @@
+class RemoveReadFromMessage < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :messages, :read, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_062406) do
+ActiveRecord::Schema.define(version: 2021_03_17_010749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,7 +107,6 @@ ActiveRecord::Schema.define(version: 2021_03_15_062406) do
     t.text "body"
     t.bigint "conversation_id", null: false
     t.bigint "user_id", null: false
-    t.boolean "read", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
